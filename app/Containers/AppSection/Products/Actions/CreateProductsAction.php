9b36164edfdf720711b6bead9a2de799fb2c934a
SreyPichh <phansreypich2015@gmail.com>
2022-04-11 15:36:24 +0700

    refactor: adding auth class in action

--- app/Containers/AppSection/Products/Actions/CreateProductsAction.php
+++ app/Containers/AppSection/Products/Actions/CreateProductsAction.php
@@ -6,6 +6,7 @@ use App\Containers\AppSection\Products\Models\Products;
 use App\Containers\AppSection\Products\Tasks\CreateProductsTask;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Parents\Requests\Request;
+use Auth;
 
 class CreateProductsAction extends Action
 {
