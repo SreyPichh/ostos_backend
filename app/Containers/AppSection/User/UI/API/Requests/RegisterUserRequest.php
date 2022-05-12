8459be365bdb7b333d4bbcf4548915131ee25bb6
SreyPichh <phansreypich2015@gmail.com>
2022-05-12 23:16:51 +0700

    update user require field

--- app/Containers/AppSection/User/UI/API/Requests/RegisterUserRequest.php
+++ app/Containers/AppSection/User/UI/API/Requests/RegisterUserRequest.php
@@ -32,9 +32,9 @@ class RegisterUserRequest extends Request
     public function rules(): array
     {
         return [
-            'email' => 'required|email|max:40|unique:users,email',
-            'password' => 'required|min:6|max:30',
-            'name' => 'required|min:2|max:50',
+//            'email' => 'required|email|max:40|unique:users,email',
+//            'password' => 'required|min:6|max:30',
+            'name' => 'required|min:1|max:64',
         ];
     }
 
