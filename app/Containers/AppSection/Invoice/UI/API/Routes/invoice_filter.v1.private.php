bb15b950dc221bda365dcfef7b692e62503b8cc4
SreyPichh <phansreypich2015@gmail.com>
2022-05-18 09:58:43 +0700

    add new filter route

---
+++ app/Containers/AppSection/Invoice/UI/API/Routes/invoice_filter.v1.private.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @apiGroup           Invoice
+ * @apiName            GetAllFilterInvoices
+ *
+ * @api                {GET} /v1/ Endpoint title here..
+ * @apiDescription     Endpoint description here..
+ *
+ * @apiVersion         1.0.0
+ * @apiPermission      none
+ *
+ * @apiParam           {String}  parameters here..
+ *
+ * @apiSuccessExample  {json}  Success-Response:
+ * HTTP/1.1 200 OK
+{
+  // Insert the response of the request here...
+}
+ */
+
+use App\Containers\AppSection\Invoice\UI\API\Controllers\Controller;
+use Illuminate\Support\Facades\Route;
+
+Route::get('', [Controller::class, 'GetAllFilterInvoices'])
+    ->name('api_invoice_get_all_filter_invoices')
+    ->middleware(['auth:api']);
+
