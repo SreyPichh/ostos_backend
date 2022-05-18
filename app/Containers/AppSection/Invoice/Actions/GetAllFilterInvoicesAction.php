9476269bcb62d7c91fad660d1560da457eb2c90e
SreyPichh <phansreypich2015@gmail.com>
2022-05-18 10:36:42 +0700

    add action class

---
+++ app/Containers/AppSection/Invoice/Actions/GetAllFilterInvoicesAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Containers\AppSection\Invoice\Actions;
+
+use App\Ship\Parents\Actions\Action;
+
+class GetAllFilterInvoicesAction extends Action
+{
+    public function run()
+    {
+        // $var = app(Task::class)->run($arg1, $arg2);
+    }
+}
