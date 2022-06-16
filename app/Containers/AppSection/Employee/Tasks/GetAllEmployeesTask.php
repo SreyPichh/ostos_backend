e74b2e5d4f09a2529356e5e4c3836018eddecf06
SreyPichh <phansreypich2015@gmail.com>
2022-06-16 08:15:37 +0700

    update disc order

--- app/Containers/AppSection/Employee/Tasks/GetAllEmployeesTask.php
+++ app/Containers/AppSection/Employee/Tasks/GetAllEmployeesTask.php
@@ -16,6 +16,6 @@ class GetAllEmployeesTask extends Task
 
     public function run()
     {
-        return $this->repository->paginate();
+        return $this->repository->orderBy('updated_at', 'desc')->paginate();
     }
 }
