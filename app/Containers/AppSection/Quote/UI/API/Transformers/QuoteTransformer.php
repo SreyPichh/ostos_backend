e1711ce364bd6fc322583b9653a488fa7bddbf98
SreyPichh <phansreypich2015@gmail.com>
2022-06-20 22:07:51 +0700

    fix transformer

--- app/Containers/AppSection/Quote/UI/API/Transformers/QuoteTransformer.php
+++ app/Containers/AppSection/Quote/UI/API/Transformers/QuoteTransformer.php
@@ -10,14 +10,14 @@ class QuoteTransformer extends Transformer
     /**
      * @var  array
      */
-    protected $defaultIncludes = [
+    protected array $defaultIncludes = [
 
     ];
 
     /**
      * @var  array
      */
-    protected $availableIncludes = [
+    protected array $availableIncludes = [
 
     ];
 
