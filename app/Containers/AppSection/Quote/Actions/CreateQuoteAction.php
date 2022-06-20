a7f7e84d0d122c43df1146c587203f755bf436ba
SreyPichh <phansreypich2015@gmail.com>
2022-06-20 22:03:37 +0700

    add action

--- app/Containers/AppSection/Quote/Actions/CreateQuoteAction.php
+++ app/Containers/AppSection/Quote/Actions/CreateQuoteAction.php
@@ -11,9 +11,17 @@ class CreateQuoteAction extends Action
 {
     public function run(Request $request): Quote
     {
-        $data = $request->sanitizeInput([
-            // add your request data here
-        ]);
+        $data = [
+            'quote_number' => $request->quote_number,
+            'date' => $request->date,
+            'due_amount' => $request->due_amount,
+            'business_id' => $request->business_id,
+            'product_data' => json_encode($request->product_data),
+            'sample_img' => $request->sample_img,
+            'signature' => $request->signature,
+            'status' => $request->status,
+            'total' => $request->total
+        ];
 
         return app(CreateQuoteTask::class)->run($data);
     }
--- app/Containers/AppSection/Quote/Actions/UpdateQuoteAction.php
+++ app/Containers/AppSection/Quote/Actions/UpdateQuoteAction.php
@@ -11,9 +11,18 @@ class UpdateQuoteAction extends Action
 {
     public function run(Request $request): Quote
     {
-        $data = $request->sanitizeInput([
+        $data = [
             // add your request data here
-        ]);
+            'quote_number' => $request->quote_number,
+            'date' => $request->date,
+            'due_amount' => $request->due_amount,
+            'business_id' => $request->business_id,
+            'product_data' => json_encode($request->product_data),
+            'sample_img' => $request->sample_img,
+            'signature' => $request->signature,
+            'status' => $request->status,
+            'total' => $request->total
+        ];
 
         return app(UpdateQuoteTask::class)->run($request->id, $data);
     }
