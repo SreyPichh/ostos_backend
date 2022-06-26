eb206410045a55c436a041f98eac066f5f2ba90b
SreyPichh <phansreypich2015@gmail.com>
2022-06-26 22:53:51 +0700

    add data

--- app/Containers/AppSection/Invoice/Models/Invoice.php
+++ app/Containers/AppSection/Invoice/Models/Invoice.php
@@ -7,7 +7,19 @@ use App\Ship\Parents\Models\Model;
 class Invoice extends Model
 {
     protected $fillable = [
-
+        'invoice_number',
+        'business_id',
+        'customer_id',
+        'po',
+        'date',
+        'due_amount',
+        'employee_data',
+        'product_data',
+        'sample_img',
+        'invoice_note',
+        'signature',
+        'status',
+        'total'
     ];
 
     protected $attributes = [
@@ -31,4 +43,13 @@ class Invoice extends Model
      * A resource key to be used in the serialized responses.
      */
     protected string $resourceKey = 'Invoice';
+
+    public function business()
+    {
+        return $this->belongsTo(Business::class, 'invoice_id', 'id');
+    }
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'invoice_id', 'id');
+    }
 }
