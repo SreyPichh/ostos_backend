5cf60342aa420015eb31dd6cb8f9104468f9f293
SreyPichh <phansreypich2015@gmail.com>
2022-07-03 21:08:08 +0700

    comment in route

--- app/Containers/AppSection/Invoice/UI/API/Routes/PublicRoute.v1.private.php
+++ app/Containers/AppSection/Invoice/UI/API/Routes/PublicRoute.v1.private.php
@@ -1,29 +1,29 @@
 <?php
-
-/**
- * @apiGroup           Invoice
- * @apiName            getAllInvoices
- *
- * @api                {GET} /v1/invoices/:id Endpoint title here..
- * @apiDescription     Endpoint description here..
- *
- * @apiVersion         1.0.0
- * @apiPermission      none
- *
- * @apiParam           {String}  parameters here..
- *
- * @apiSuccessExample  {json}  Success-Response:
- * HTTP/1.1 200 OK
-{
-  // Insert the response of the request here...
-}
- */
-
-use App\Containers\AppSection\Invoice\UI\API\Controllers\Controller;
-use Illuminate\Support\Facades\Route;
-
-Route::get('invoices_public/{id}', [Controller::class, 'findInvoiceById'])
-    ->name('api_invoice_get_all_invoices')
-    ->middleware('auth:api')
-    ->middleware(['auth:api']);
-
+//
+///**
+// * @apiGroup           Invoice
+// * @apiName            getAllInvoices
+// *
+// * @api                {GET} /v1/invoices/:id Endpoint title here..
+// * @apiDescription     Endpoint description here..
+// *
+// * @apiVersion         1.0.0
+// * @apiPermission      none
+// *
+// * @apiParam           {String}  parameters here..
+// *
+// * @apiSuccessExample  {json}  Success-Response:
+// * HTTP/1.1 200 OK
+//{
+//  // Insert the response of the request here...
+//}
+// */
+//
+//use App\Containers\AppSection\Invoice\UI\API\Controllers\Controller;
+//use Illuminate\Support\Facades\Route;
+//
+//Route::get('invoices_public/{id}', [Controller::class, 'findInvoiceById'])
+//    ->name('api_invoice_get_all_invoices')
+//    ->middleware('auth:api')
+//    ->middleware(['auth:api']);
+//
