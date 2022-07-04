ff657dfdf28a5d9afecd8470b8733f0613190835
SreyPichh <phansreypich2015@gmail.com>
2022-07-04 13:34:13 +0700

    update search null

--- app/Containers/AppSection/Invoice/Actions/GetAllInvoicesAction.php
+++ app/Containers/AppSection/Invoice/Actions/GetAllInvoicesAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Containers\AppSection\Invoice\Actions;
 
+use App\Containers\AppSection\Invoice\Models\Invoice;
 use App\Containers\AppSection\Invoice\Tasks\GetAllInvoicesTask;
 use App\Ship\Parents\Actions\Action;
 use App\Ship\Parents\Requests\Request;
@@ -10,6 +11,10 @@ class GetAllInvoicesAction extends Action
 {
     public function run(Request $request)
     {
+        if($request->search == 'customer_id:null')
+        {
+            return Invoice::where('customer_id', null)->paginate();
+        }
         return app(GetAllInvoicesTask::class)->addRequestCriteria()->run();
     }
 }
