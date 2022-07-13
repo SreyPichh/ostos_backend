486a4b752a021c4267b997c57484048af94fcb2f
SreyPichh <phansreypich2015@gmail.com>
2022-07-13 11:17:39 +0700

    add quote

--- app/Containers/AppSection/Quote/Models/Quote.php
+++ app/Containers/AppSection/Quote/Models/Quote.php
@@ -7,6 +7,20 @@ use App\Ship\Parents\Models\Model;
 class Quote extends Model
 {
     protected $fillable = [
+        'quote_number',
+        'business_id',
+        'customer_id',
+        'po',
+        'date',
+        'due_amount',
+        'employee_data',
+        'product_data',
+        'sample_img',
+        'quote_note',
+        'signature',
+        'status',
+        'total',
+        'customer_info'
 
     ];
 
