f19f3ac0aee6e3d21e59a468278a3ebdf354a4b9
SreyPichh <phansreypich2015@gmail.com>
2022-07-16 21:28:19 +0700

    add quote

--- app/Containers/AppSection/Quote/Actions/CreateQuoteAction.php
+++ app/Containers/AppSection/Quote/Actions/CreateQuoteAction.php
@@ -11,9 +11,23 @@ class CreateQuoteAction extends Action
 {
     public function run(Request $request): Quote
     {
-        $data = $request->sanitizeInput([
+        $data = [
             // add your request data here
-        ]);
+            'quote_number' => $request->invoice_number,
+            'business_id' => $request->business_id,
+            'customer_id' => $request->customer_id,
+            'po' => $request->po,
+            'date' => $request->date,
+            'due_amount' => $request->due_amount,
+            'employee_data' => json_encode($request->employee_data),
+            'product_data' => json_encode($request->product_data),
+            'sample_img' => $request->sample_img,
+            'quote_note' => $request->invoice_note,
+            'signature' => $request->signature,
+            'status' => $request->status,
+            'total' => $request->total,
+            'customer_info' => json_encode($request->customer_info)
+        ];
 
         return app(CreateQuoteTask::class)->run($data);
     }
--- app/Containers/AppSection/Quote/UI/API/Transformers/QuoteTransformer.php
+++ app/Containers/AppSection/Quote/UI/API/Transformers/QuoteTransformer.php
@@ -10,14 +10,14 @@ class QuoteTransformer extends Transformer
     /**
      * @var  array
      */
-    protected $defaultIncludes = [
+    protected array $defaultIncludes = [
 
     ];
 
     /**
      * @var  array
      */
-    protected $availableIncludes = [
+    protected array $availableIncludes = [
 
     ];
 
@@ -26,11 +26,24 @@ class QuoteTransformer extends Transformer
         $response = [
             'object' => $quote->getResourceKey(),
             'id' => $quote->getHashedKey(),
+            'quote_number' => $quote->invoice_number,
+            'business_id' => $quote->business_id,
+            'customer_id' => $quote->customer_id,
+            'po' => $quote->po,
+            'date' => $quote->date,
+            'due_amount' => $quote->due_amount,
+            'employee_data' => json_decode($quote->employee_data),
+            'product_data' => json_decode($quote->product_data),
+            'sample_img' => $quote->sample_img,
+            'quote_note' => $quote->invoice_note,
+            'signature' => $quote->signature,
+            'status' => $quote->status,
+            'total' => $quote->total,
+            'customer_info' => json_decode($quote->customer_info),
             'created_at' => $quote->created_at,
             'updated_at' => $quote->updated_at,
             'readable_created_at' => $quote->created_at->diffForHumans(),
             'readable_updated_at' => $quote->updated_at->diffForHumans(),
-
         ];
 
         return $response = $this->ifAdmin([
